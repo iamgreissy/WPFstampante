6d752845858c6475c8e2862462eb3b7e0ada8343
Regreis Syziu <130826811+iamgreissy@users.noreply.github.com>
2023-12-13 08:09:37 +0100

    Update Program.cs

--- WPFstampante/WPFstampante/Program.cs
+++ WPFstampante/WPFstampante/Program.cs
@@ -9,10 +9,22 @@ public class Pagina
 
     public Pagina(int c, int m, int y, int b)
     {
-        C = c;
-        M = m;
-        Y = y;
-        B = b;
+        if (IsValidColoreValue(c) && IsValidColoreValue(m) && IsValidColoreValue(y) && IsValidColoreValue(b))
+        {
+            C = c;
+            M = m;
+            Y = y;
+            B = b;
+        }
+        else
+        {
+            throw new ArgumentException("Valore del colore non valido. Deve essere compreso tra 0 e 100.");
+        }
+    }
+
+    private bool IsValidColoreValue(int value)
+    {
+        return value >= 0 && value <= 100;
     }
 }
 
@@ -24,14 +36,12 @@ public class Stampante
     public int B { get; private set; }
     public int Fogli { get; private set; }
 
-    // COSTRUTTORE
     public Stampante()
     {
         C = M = Y = B = 100; // assumendo che il valore predefinito sia 100 per ogni colore
         Fogli = 200;
     }
 
-    // metodo
     public bool Stampa(Pagina p)
     {
         if (Fogli > 0 && C >= p.C && M >= p.M && Y >= p.Y && B >= p.B)
@@ -47,21 +57,66 @@ public class Stampante
         return false;
     }
 
-    public int Inch(string colore)
+    public int StatoInchiostro(Colore colore)
     {
-        // Supponendo che si voglia ottenere il livello di inchiostro del colore specificato
-        switch (colore.ToUpper())
+        switch (colore)
         {
-            case "C":
+            case Colore.C:
                 return C;
-            case "M":
+            case Colore.M:
                 return M;
-            case "Y":
+            case Colore.Y:
                 return Y;
-            case "B":
+            case Colore.B:
                 return B;
             default:
-                throw new ArgumentException("Colore specificato non valido.");
+                throw new ArgumentException("Colore non valido.");
         }
     }
+
+    public int StatoCarta()
+    {
+        return Fogli;
+    }
+
+    public void SostituisciColore(Colore colore)
+    {
+        switch (colore)
+        {
+            case Colore.C:
+                C = 100;
+                break;
+            case Colore.M:
+                M = 100;
+                break;
+            case Colore.Y:
+                Y = 100;
+                break;
+            case Colore.B:
+                B = 100;
+                break;
+            default:
+                throw new ArgumentException("Colore non valido.");
+        }
+    }
+
+    public void AggiungiCarta(int qta)
+    {
+        if (qta > 0)
+        {
+            Fogli = Math.Min(Fogli + qta, 200);
+        }
+        else
+        {
+            throw new ArgumentException("La quantità di carta deve essere maggiore di zero.");
+        }
+    }
+}
+
+public enum Colore
+{
+    C,
+    M,
+    Y,
+    B
 }
